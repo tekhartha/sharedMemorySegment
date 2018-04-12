45935524d468476643f01c9056a59f00ceab87e0
j. nguyen <ljubenfro@gmail.com>
2018-04-11 18:44:48 -0700

    Add files via upload

--- shmc1.cpp
+++ shmc1.cpp
@@ -12,9 +12,7 @@
 #include <stdio.h>
 #include <memory.h>
 
-
 using namespace std;
-
 
 CLASS *class_ptr;
 void *memptr;
@@ -32,40 +30,54 @@ main(int argc, char* argv[])
 	
 	pname = argv[0];
 	sscanf (argv[1], "%d", &shmid);
+	/*
+		shmat(id, address, flag)
+		Attach shared memory segment identified by ID to shared memory address.
+	*/
 	memptr = shmat (shmid, (void *)0, 0);
 	if (memptr == (char *)-1 ) {
 		rpterror ("shmat failed");
 		exit(2);
 	}
 
-	class_ptr = (struct CLASS *)memptr;
+	class_ptr = (struct CLASS *)memptr; // create pointer of type CLASS to memory segment
 
 	sell_seats();
-
+	
+	/*
+		shmdt(address)
+		Detach the shared memory segment at given address
+		Should return 0 on success
+	*/
 	ret = shmdt(memptr);
 	exit(0);
 }
 
+/*
+	
+*/
 void sell_seats()
 {
 	int all_out = 0;
-	srand ( (unsigned) getpid() );
-	while ( !all_out) { /* loop to sell all seats */
+	srand ( (unsigned) getpid() ); // initialize random number generator with a seed
+	while ( !all_out) { // loop to sell all seats
 		if (class_ptr->seats_left > 0) {
-			sleep ( (unsigned)rand()%5 + 1);
-			class_ptr->seats_left--;
+			sleep ( (unsigned)rand()%5 + 1); // thread sleeps for given number of seconds
+			class_ptr->seats_left--; // decrement number of seats ("selling")
 			sleep ( (unsigned)rand()%5 + 1);
 			cout << pname << " SOLD SEAT -- " << class_ptr->seats_left << " left" << endl;
 		}
 		else {
-			all_out++;
+			all_out++; // increment all_out to break out of loop
 			cout << pname << " sees no seats left" << endl;
 		}
 		sleep ( (unsigned)rand()%10 + 1);
 	}
 }
 
-
+/*
+	Print formatted error message.
+*/
 void rpterror(char* string)
 {
 	char errline[50];
@@ -73,3 +85,4 @@ void rpterror(char* string)
 	sprintf (errline, "%s %s", string, pname);
 	perror (errline);
 }
+
--- shmp1.cpp
+++ shmp1.cpp
@@ -14,9 +14,9 @@
 
 using namespace std;
 
-CLASS myclass = { "1001", "120186", "Operating Systems", 15 };
+CLASS myclass = { "1001", "120186", "Operating Systems", 15 }; // Class Number, Date, Title, Seats left
 
-#define NCHILD	3
+#define NCHILD	3 // Define constant with global file scope
 
 int	shm_init( void * );
 void	wait_and_wrap_up( int [], void *, int );
@@ -24,54 +24,77 @@ void	rpterror( char *, char * );
 
 main(int argc, char *argv[])
 {
-	int child[NCHILD], i, shmid, semid;
-	void	*shm_ptr;
+	int	child[NCHILD], i, shmid, semid; // declare ints
+	void	*shm_ptr; // pointer to shared memory location
 	char	ascshmid[10], ascsemid[10], pname[14];
 
-	strcpy (pname, argv[0]);
-	shmid = shm_init(shm_ptr);
+	strcpy (pname, argv[0]); // strcpy(destination, source)
+	shmid = shm_init(shm_ptr); // create shared memory segment ID
 	sprintf (ascshmid, "%d", shmid);
 
 
-	for (i = 0; i < NCHILD; i++) {
-		child[i] = fork();
+	for (i = 0; i < NCHILD; i++) { // create 3 child processes
+		child[i] = fork(); // put PIDS of child processes into child array
 		switch (child[i]) {
 		case -1:
 			rpterror ("fork failure", pname);
 			exit(1);
 		case 0:
-			sprintf (pname, "shmc%d", i+1);
-			execl("shmc1", pname, ascshmid, (char *)0);
+			sprintf (pname, "shmc%d", i+1); // print number of child process
+			execl("shmc1", pname, ascshmid, (char *)0); // execl(path, args...) execute shmc1
 			perror ("execl failed");
 			exit (2);
 		}
 	}
-	wait_and_wrap_up (child, shm_ptr, shmid);	
+	wait_and_wrap_up (child, shm_ptr, shmid); // Remove shared memory segments
 }
 
-	
+/*
+	Create identifier for shared memory segment
+*/	
 int shm_init(void *shm_ptr)
 {
 	int	shmid;
+	/*
+		shmget(key, size, flag)
+		Allocate System V shared memory segment.
+		-ftok: return key based on (path, id)
+		-sizeof(CLASS): size of memory segment
+		-0600: octal permission
+		-IPC_CREAT: create new segment
+	*/
 	shmid = shmget(ftok(".",'u'), sizeof(CLASS), 0600 | IPC_CREAT);
 	if (shmid == -1) {
 		perror ("shmget failed");
 		exit(3);
 	}
+	/*
+		shmat(id, address, flag)
+		Attach shared memory segment identified by ID to shared memory address.
+	*/
 	shm_ptr = shmat(shmid, (void * ) 0, 0);
 	if (shm_ptr == (void *) -1) {
 		perror ("shmat failed");
 		exit(4);
 	}
+	/*
+		memcpy(source ptr, destination ptr, count)
+		Copy [count] bytes from source mem location to destination mem location. 
+		
+	*/
 	memcpy (shm_ptr, (void *) &myclass, sizeof(CLASS) );
 	return (shmid);
 }
 
+
+/*
+	Remove the shared memory segment (children) associated with the shmid.
+*/
 void wait_and_wrap_up(int child[], void *shm_ptr, int shmid)
 {
 	int wait_rtn, w, ch_active = NCHILD;
 
-	while (ch_active > 0) {
+	while (ch_active > 0) { // wait for all children to terminate
 		wait_rtn = wait( (int *)0 );
 		for (w = 0; w < NCHILD; w++)
 			if (child[w] == wait_rtn) {
@@ -80,15 +103,28 @@ void wait_and_wrap_up(int child[], void *shm_ptr, int shmid)
 			}
 	}
 	cout << "Parent removing shm" << endl;
+	/*
+		shmdt(address)
+		Detach the shared memory segment at given address
+	*/
 	shmdt (shm_ptr);
+	/*
+		shmcl(id, control operation, shmid_ds structure)
+		Perform control operation on the shared mem segment that id refers to.
+		-IPC_RMID: destroy segment
+	*/
 	shmctl (shmid, IPC_RMID, (struct shmid_ds *) 0);
 	exit (0);
 }
 
 
+/*
+	Print formatted error message.
+*/
 void rpterror(char *string, char *pname)
 {
 	char errline[50];
 	sprintf (errline, "%s %s", string, pname);
 	perror (errline);
 }
+
